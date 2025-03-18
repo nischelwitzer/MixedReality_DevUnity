0e0e5f46e5ef08209b0b642658788ac7392c5aef
Alexander Nischelwitzer <nischelwitzer@users.noreply.github.com>
2025-03-18 10:19:29 +0100

    Update DMTEventHelpers.cs

--- scripts/DMTEventHelpers.cs
+++ scripts/DMTEventHelpers.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class DMTEventHelpers : MonoBehaviour
 {
+
+    private bool myToggle = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("##### DMTEventHelpers: Start");
+    }
+
+    public void HelperToggle()
+    {
+        myToggle = !myToggle;
+        this.gameObject.SetActive(myToggle);
     }
 
     // Update is called once per frame
